084a6f7d73e39b1b28603d0743afa5468c59ef68
rodridiaz <rodridm@gmail.com>
2018-06-14 20:03:23 +0200

    install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { UserCreateComponent } from './users/user-create/user-create.component';
@@ -12,7 +14,9 @@ import { UserCreateComponent } from './users/user-create/user-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
