a577d00d2567154b80e35360751ba7de7231a882
rodridiaz <rodridm@gmail.com>
2018-06-15 10:54:03 +0200

    fetch initial users

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('hello from express');
+app.use('/api/users', (req, res, next) => {
+  const users = [
+    {
+      id: 'sad213asd324',
+      name: 'test name',
+      email: 'asdsad@asdsadf.com'
+    },
+    {
+      id: 'sdfsdf687s6d',
+      name: 'test name 2',
+      email: 'asdsad@asdsadf.com'
+    }
+  ];
+  res.status(200).json({
+    message: 'Users fetched succesfully!',
+    users: users
+  });
 });
 
 module.exports = app;
