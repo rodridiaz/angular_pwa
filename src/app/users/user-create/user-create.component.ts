bd451b2dec4d34b56f9c338956f33e9c2d96f122
rodridiaz <rodridm@gmail.com>
2018-06-16 12:48:40 +0200

    add image controls to store the image

--- src/app/users/user-create/user-create.component.ts
+++ src/app/users/user-create/user-create.component.ts
@@ -28,6 +28,9 @@ export class UserCreateComponent implements OnInit {
       }),
       'email': new FormControl(null, {
         validators: [Validators.required]
+      }),
+      'image': new FormControl(null, {
+        validators: [Validators.required]
       })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -52,6 +55,12 @@ export class UserCreateComponent implements OnInit {
         this.userId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
   }
 
   onSaveUser() {
